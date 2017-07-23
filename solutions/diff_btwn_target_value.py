b95e2d2d67eabef2169cb5c751626d49466c39f0
Aleksei <aleksei.git@gmail.com>
2017-07-23 16:01:54 -0400

    Used defaultdict for interview task

--- solutions/diff_btwn_target_value.py
+++ solutions/diff_btwn_target_value.py
@@ -1,9 +1,12 @@
+from collections import defaultdict
+
+
 def some_function(target, values):
     if not values:
         return None
     results = {elem: abs(target - elem) for elem in values}
-    new_results = {}
+    new_results = defaultdict(list)
     for k, v in results.items():
-        new_results.setdefault(v, []).append(k)
+        new_results[v].append(k)
     closest = min(new_results)
     return min(new_results.get(closest))
